609296ae177766682452c4c7857fae0fa5cd1b38
Akhil Waghmare <akhilwaghmare@gmail.com>
2019-06-03 20:12:29 -0400

    Implemented three different ways to conform to dark mode

--- Dark Mode Example/ViewController.swift
+++ Dark Mode Example/ViewController.swift
@@ -9,10 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label1: UILabel = {
+        let label = UILabel()
+        label.text = "I use UI Element Colors"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.layer.cornerRadius = 4.0
+        label.textColor = .label
+        label.backgroundColor = .secondarySystemBackground
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let label2: UILabel = {
+        let label = UILabel()
+        label.text = "I use Adaptable Colors"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.layer.cornerRadius = 4.0
+        label.textColor = .systemOrange
+        label.backgroundColor = .systemBrown
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let label3: UILabel = {
+        let label = UILabel()
+        label.text = "I use Color Assets"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.layer.cornerRadius = 4.0
+        label.textColor = UIColor(named: "customTextColor")
+        label.backgroundColor = UIColor(named: "customBackgroundColor")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12.0
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "iOS 13 Dark Mode"
+        view.backgroundColor = .systemBackground
+        setupViews()
+    }
+    
+    
+    
+    private func setupViews() {
+        stackView.addArrangedSubview(label1)
+        stackView.addArrangedSubview(label2)
+        stackView.addArrangedSubview(label3)
+        view.addSubview(stackView)
+        
+        stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -32).isActive = true
+        
     }
 
 
